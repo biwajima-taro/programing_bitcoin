e3053bd426fce77c9c550a2ef123d9a1d2917b38
biwajima_taro <taro.biwajima@gmail.com>
2019-12-08 21:49:26 +0900

    add a test case

--- chapter1/test/test_field_element.py
+++ chapter1/test/test_field_element.py
@@ -14,9 +14,16 @@ def test_add():
     assert actual == expected
 
 
-def test_illegal():
+def test_add_illegal():
     '''test add in finite field.'''
     element1 = FieldElement(num=3, prime=13)
     element2 = FieldElement(num=6, prime=20)
     with pytest.raises(ValueError):
         actual = element1+element2
+
+def test_default_value():
+	'''test with illegal constructor arguments'''
+	with pytest.raises(ValueError):
+		tmp=FieldElement(num=-123,prime=123)
+	with pytest.raises(ValueError):
+		tmp2=FieldElement(num=123,prime=-123)
