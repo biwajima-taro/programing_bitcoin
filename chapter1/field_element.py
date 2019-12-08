492f3b9b52539f1c12d659c5f0f1d145fc9b110f
biwajima_taro <taro.biwajima@gmail.com>
2019-12-08 20:54:21 +0900

    rewrite according to pylint

--- chapter1/field_element.py
+++ chapter1/field_element.py
@@ -1,30 +1,33 @@
 
 class FieldElement:
-	'class representing a finite filed element'
-	def __init__(self,num,prime):
-		if num>prime or num<0:	raise ValueError("illegal input")		
-		self.num=num
-		self.prime=prime
+    'class representing a finite filed element'
 
-	def __eq__(self,other):
-		if other is None:return False
-		return self.num=oter.num and self.prime==other.prime
-	
-	def __add__(self,other):
-		if self.prime !=other.prime:riase ValueError("cannot two add numbers in diffrent fields")
-		num=(self.num+other.num)%self.prime
-		return self.__class__(num,self.prime)
-	
-	def __sub__(self,other):
-		if self.prime !=other.prime:raise ValueError("cannot subtract numbers in diffrent fields")
-		num=(self.num-other.num)%self.prime
-		return self.__class__(num,self.prime)
+    def __init__(self, num, prime):
+        if num > prime or num < 0:
+            raise ValueError("illegal input")
+        self.num = num
+        self.prime = prime
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise ValueError("cannot two add numbers in diffrent fields")
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise ValueError("cannot subtract numbers in diffrent fields")
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __ne__(self, other):
+        return not self == other
 
 
-	def __ne__(self,other):
-		return not (self==other) 
-		#if other is None:
-		#	return False
-		#return not self.__eq__(other
-if __name__=="__main__":
-	print("test")
+if __name__ == "__main__":
+    print("test")
