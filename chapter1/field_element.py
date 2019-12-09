f31be3402e627417b3c6d7b144e0a621223c73cb
biwajima_taro <taro.biwajima@gmail.com>
2019-12-09 21:58:01 +0900

    rewrite

--- chapter1/field_element.py
+++ chapter1/field_element.py
@@ -1,7 +1,7 @@
 
 
 class FieldElement:
-    """class representing a finite filed element"""
+    """class representing a finite filed element."""
 
     def __init__(self, num: int, prime: int):
         """constructor."""
@@ -30,10 +30,27 @@ class FieldElement:
     def __ne__(self, other):
         return not self == other
 
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("cannot divide in diffrent fields")
+        num = self.num*pow(other.num, self.prime-2, self.prime) % self.prime
+        return self.__class__(num=num, prime=self.prime)
+
+    def __pow__(self, exponent):
+        n=exponent % (self.prime-1)
+        num=pow(self.num,n,self.prime)
+        #num = (self.num**exponent) % self.prime
+        return self.__class__(num=num, prime=self.prime)
+
     def __repr__(self):
         return "FieldElement(num={},prime={})".format(self.num, self.prime)
 
-
 if __name__ == "__main__":
+    import time
     tmp = FieldElement(12, 13)
-    print(tmp)
+    #ex7
+    for p in [7,11,17,31]:
+        tmp=[ pow(ele,p-1,p) for ele in range(1,p)]
+        print(tmp)
+        time.sleep(2)
+
