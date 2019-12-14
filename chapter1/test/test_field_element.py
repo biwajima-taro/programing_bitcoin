49cc36df9a412b34dbd17f02b0c1c531b1fb9d05
biwajima_taro <taro.biwajima@gmail.com>
2019-12-14 22:47:45 +0900

    update test cases

--- chapter1/test/test_field_element.py
+++ chapter1/test/test_field_element.py
@@ -1,24 +1,29 @@
-from chapter1.field_element import FieldElement
-import pytest
-import os
 import sys
+import os
 sys.path.append(os.pardir)
 sys.path.append("../../")
+from chapter1.field_element import FieldElement
+import pytest
+
 
 
 def test_multiply():
     element1 = FieldElement(num=3, prime=13)
     element2 = FieldElement(num=6, prime=13)
     expected = FieldElement(num=5, prime=13)
-    assert expected == element1*element2
-
-
+    actual = element1 * element2
+    assert expected == actual  # element1*element2
+    not_expected = FieldElement(num=3, prime=13)
+    assert not_expected != actual
+    
 def test_exponetiation():
     element1 = FieldElement(num=3, prime=13)
     x = 3
     expected = FieldElement(num=1, prime=13)
-    assert expected == element1**x
-
+    actual = element1**x
+    assert expected == actual
+    not_expected = FieldElement(num=2, prime=13)
+    assert not_expected !=actual
 
 def test_add():
     '''test add in finite field.'''
@@ -27,6 +32,8 @@ def test_add():
     actual = element1 + element2
     expected = FieldElement(num=9, prime=13)
     assert actual == expected
+    not_expected = FieldElement(num=3, prime=13)
+    assert not_expected != actual
 
 
 def test_add_illegal():
@@ -34,17 +41,16 @@ def test_add_illegal():
     element1 = FieldElement(num=3, prime=13)
     element2 = FieldElement(num=6, prime=20)
     with pytest.raises(ValueError):
-        actual = element1+element2
+        element1+element2
 
 
 def test_default_value():
     '''test with illegal constructor arguments'''
     with pytest.raises(ValueError):
-        tmp = FieldElement(num=-123, prime=123)
+        FieldElement(num=-123, prime=123)
     with pytest.raises(ValueError):
-        tmp2 = FieldElement(num=123, prime=-123)
+        FieldElement(num=123, prime=-123)
     with pytest.raises(ValueError):
-        tmp3 = FieldElement(num=123.12, prime=1233)
-
+        FieldElement(num=123.12, prime=1233)
     with pytest.raises(ValueError):
-        tmp4 = FieldElement(num=34, prime=123.123)
+        FieldElement(num=34, prime=123.123)
