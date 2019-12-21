6c31dcfd4d72fff25ee6265cc93efcc0e12b3831
shibaken2017 <kentaro.shibata@datasection.co.jp>
2019-12-22 00:18:16 +0900

    private key object

---
+++ chapter1/private_key.py
@@ -0,0 +1,21 @@
+from s256 import S256Point
+G = 123
+
+G: S256Point = S256Point(12, 12)
+
+
+class PrivateKey:
+
+    def __init__(self, secret: int):
+        """test"""
+        self.secret: int = secret
+        self.point: S256Point = secret*G
+
+    def hex(self) -> str:
+        # {:x}で16進数にする
+        return "{:x}".format(self.secret).zfill(64)
+
+
+
+if __name__=="__main__":
+   tnp= PrivateKey()
