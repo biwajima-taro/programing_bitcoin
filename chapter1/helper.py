fd74af242377e3a5e6034e91e314c6890b85cac3
biwajima_taro <taro.biwajima@gmail.com>
2019-12-24 23:29:11 +0900

    helper functions

---
+++ chapter1/helper.py
@@ -0,0 +1,25 @@
+import hashlib
+
+
+def encode_base58(s: bytes) -> str:
+    count: int = 0
+    for c in s:
+        if c == 0:
+            count += 1
+        else:
+            break
+    num: int = int.from_bytes(s, "big")
+    prefix: str = "1"
+    result: str = ""
+    # TODO:write rest of the function
+    return result
+
+
+def hash256(s: str) -> str:
+    # https://www.pebblewind.com/entry/2018/05/05/230100
+    # s.encode('utf-8')にする必要がある？
+    return hashlib.new("rpemd160", hashlib.sha256(s).digest()).digest()
+
+
+def encode_base58_checksum(b):
+    return encode_base58(b+hash256(b)[:4])
