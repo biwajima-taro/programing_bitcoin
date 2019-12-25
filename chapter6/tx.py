706c2f9bb8c565ec56f15eb07e951aad5c03d91f
biwajima-taro <taro.biwajima@gmail.com>
2019-12-25 23:45:32 +0900

    add type hint

--- chapter6/tx.py
+++ chapter6/tx.py
@@ -1,18 +1,19 @@
 from chapter1.helper import little_endiant_to_int, read_variant
 from chapter6.tx_in import TxIn
+from typing import List
 
 
 class Tx:
-    def __init__(self, version, tx_ins, tx_outs, locktime, testnet=False):
+    def __init__(self, version: int, tx_ins: List, tx_outs: List, locktime: int, testnet=False):
         self.version = version
-        self.tx_ins = tx_ins
-        self.tx_outs = tx_outs
-        self.locktime = locktime
+        self.tx_ins: List = tx_ins
+        self.tx_outs: List = tx_outs
+        self.locktime: int = locktime
         self.testnet = testnet
     # https://blog.pyq.jp/entry/Python_kaiketsu_190205
-   
+
     @classmethod
-    def parse(cls, s:bytes, testnet=False):
+    def parse(cls, s: bytes, testnet=False):
         version = little_endiant_to_int(s.read(4))
         num_inputs = read_variant(s)
         inputs = []
