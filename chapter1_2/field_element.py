f65ae31738c49354ee623672e5d62c3eb8b8e64f
biwajima-taro <taro.biwajimap@gmail.com>
2020-03-02 22:02:12 +0900

    field element(rewriteing)

---
+++ chapter1_2/field_element.py
@@ -0,0 +1,37 @@
+
+class FieldElement:
+    def __init__(self, num: int, prime: int):
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self) -> str:
+        return f"FieldElement_{self.prime}({self.num})"
+
+    def __eq__(self, other) -> bool:
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other: FieldElement) -> bool:
+        return not (self == other)
+
+    def __add__(self, other: FieldElement) -> FieldElement:
+        if self.__check(other):
+            num = (self.num+other.num) % self.prime
+            return self.__class__(num, self.prime)
+
+    def __sub__(self, other: FieldElement) -> FieldElement:
+        if self.__check(other):
+            num = (self.num-other.num) % self.prime
+            return self.__class__(num, self.prime)
+
+    def __check(self, other: FieldElement) -> bool:  
+        if self.prime == other.prime:
+            return True
+         raise TypeError("two number must be the same prime")
+
+
+if __name__ == "__main__":
+    # TODO execise2
+    # TODO unittest
+    print("hoge")
