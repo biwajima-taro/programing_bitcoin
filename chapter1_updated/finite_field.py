897bac9e6a6184c429344f91e392907f1c4bc52d
owari-taro <taro.biwajima@gmail.com>
2020-04-27 21:34:18 +0900

    add sub,mul

--- chapter1_updated/finite_field.py
+++ chapter1_updated/finite_field.py
@@ -20,8 +20,40 @@ class FiniteField:
         if self.prime != other.prime:
             raise ValueError("prime must be same")
         num = (self.num+other.num) % self.prime
+        # TODO:what will hapen when subclass use self.__class__?
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise ValueError("prime must be same")
+        num = (self.num-other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise ValueError("")
+        num = (self.num*other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __pow__(self, exponent: int):
+        num = (self.num**exponent) % self.prime
         return self.__class__(num, self.prime)
 
 
 if __name__ == "__main__":
     print(repr(FiniteField(3, 100)))
+    num_list = [1, 2, 3, 4, 5, 7, 9, 13, 18]
+    for num in num_list:
+        tmp_set = set()
+        for i in range(19):
+            tmp_set.add((i*num) % 19)
+        print(tmp_set)
+
+    print("\n\n")
+    ###############################
+    num_list=[7,13,31]
+    for prime in num_list:
+        tmp_set=set()
+        for num in range(1,prime):
+            tmp_set.add((num**(prime-1))%prime)
+        print(tmp_set)
