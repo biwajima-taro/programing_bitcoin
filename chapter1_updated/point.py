4153a3068f29f30f2a1005162415ab6b81a360cb
owari-taro <taro.biwajima@gmail.com>
2020-05-01 18:48:32 +0900

    add method

--- chapter1_updated/point.py
+++ chapter1_updated/point.py
@@ -18,8 +18,11 @@ class Point:
             return self
         if self.x == other.x and self.y != other.y:
             return self.__class__(None, None, self.a, self.b)
-        
-
+        if self.x != other.x:
+            s = (other.y-self.y)/(other.x-self.x)
+            x = s**2-self.x-other.x
+            y = s*(self.x-x)-self.y
+            return self.__class__(x, y, self.a, self.b)
 
     def __eq__(self, other):
         return self.a == other.a and self.b == other.b and self.x == other.x \
