b5f4798fa620627877e9e88a96d07162a53fc7cc
owari-taro <taro.biwajima@gmail.com>
2020-05-04 23:07:52 +0900

    checksum

--- chapter1_updated/helper.py
+++ chapter1_updated/helper.py
@@ -1,9 +1,11 @@
+import hashlib
+
 BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 
 def encode_base58(s: bytes) -> str:
     # count how many 0 are at start
-    count = 0cv
+    count = 0
     for c in s:
         if c == 0:
             count += 1
@@ -16,3 +18,13 @@ def encode_base58(s: bytes) -> str:
             num, mod = divmod(num, 58)
             result = BASE58_ALPHABET[mod]+result
         return prefix+result
+
+
+def hash256(s):
+    '''two rounds of sha256'''
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+
+def encode_base58_checksum(adress: nutes):
+    return encode_base58(adress+hash256(adress)[:4])
+
