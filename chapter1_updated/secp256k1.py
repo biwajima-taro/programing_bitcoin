612827f051aea93bf299e026abeefd4b438b4e34
owari-taro <taro.biwajima@gmail.com>
2020-05-04 23:21:34 +0900

    method to create address with checksum

--- chapter1_updated/secp256k1.py
+++ chapter1_updated/secp256k1.py
@@ -1,13 +1,15 @@
 from point import Point
 from field_element import FieldElement
-N = 10**3
-A = None
-B = None
+from helper import hash160, encode_base58_checksum
+A = 0
+B = 7
+P = 2**256 - 2**32 - 977
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
 
 
 class S256Field(FieldElement):
-    def __init__(self, num, prime):
-        super().__init__(num=num, prime=prime)
+    def __init__(self, num):
+        super().__init__(num=num, prime=P)
 
     def __repr__(self):
         return "{:x}".format()
@@ -15,8 +17,19 @@ class S256Field(FieldElement):
 
 class S256Point(Point):
     def __init__(self, x, y):
-        a, b = S256Point(A), S256Point(B)
-        super().__init__(x=S256Point(x), y=S256Point(y), a=a, b=b)
+        a, b = S256Field(A), S256Field(B)
+        super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
 
-    def sec(self):
-        return b"\x04"+self.x.to_bytes(32,"big")+self.y.to_bytes(32,"big")
+    def sec(self, compressed=True):
+        return b"\x04"+self.x.to_bytes(32, "big")+self.y.to_bytes(32, "big")
+
+    def hash160(self, compressed=True, testnet=False):
+        return hash160(self.sec(compressed))
+
+    def address(self, compressed=True, testnet=False):
+        h160 = self.hash160(compressed)
+        if testnet:
+            prefix = b"\x6f"
+        else:
+            prefix = b"\x00"
+        return encode_base58_checksum(prefix+h160)
