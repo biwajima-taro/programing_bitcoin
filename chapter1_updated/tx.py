ba5234e82cf626b23e9ade62619c0d1781a0f01f
owari-taro <taro.biwajima@gmail.com>
2020-05-06 23:12:43 +0900

    parse locktime at tx.py

--- chapter1_updated/tx.py
+++ chapter1_updated/tx.py
@@ -1,5 +1,7 @@
 from helper import little_endian_to_int, read_variant
 from transaction_input import TxIn
+from transaction_output import TxOut
+
 
 class Tx:
     def __init__(self, version, tx_ins, tx_outs, locktime, testnet=False):
@@ -10,7 +12,14 @@ class Tx:
     def parse(cls, s, testnet=False):
         version = little_endian_to_int(s.read(4))
         num_of_input = read_variant(s)
-        inputs=[]
-        for _ in range(num_inputs):
-            innputs.append(TxIn.parse(s))
+        inputs = []
+        for _ in range(num_of_input):
+            inputs.append(TxIn.parse(s))
+
+        num_of_output = read_variant(s)
+        outputs = []
+        for _ in range(num_of_output):
+            outputs.append(TxOut.parse(s))
+
+        locktime = little_endian_to_int(s.read())
         return cls(version, None, None, None, testnet)
