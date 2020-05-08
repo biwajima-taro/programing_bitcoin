ec4da221551317ee804aa902bbca9196f4774bca
owari-taro <taro.biwajima@gmail.com>
2020-05-08 23:21:55 +0900

    encode variant.py

--- chapter1_updated/helper.py
+++ chapter1_updated/helper.py
@@ -46,4 +46,16 @@ def read_variant(s):
     i = s.read(1)[0]
     if i == 0xfd:
         return little_endian_to_int(s.read(2))
-    
+
+
+def encode_variant(length_: int):
+    if length_ < 0xfd:
+        return bytes([length_])
+    elif length_ < 0x10000:
+        return b"\xfd"+int_to_little_endian(length_, 2)
+    elif length_ < 0x100000000:
+        return b"\xfe"+int_to_little_endian(length_, 4)
+    elif length_ < 0x10000000000000000:
+        return b"\xff"+int_to_little_endian(length_, 8)
+    else:
+        raise ValueError(f"integer too large {length_}")
