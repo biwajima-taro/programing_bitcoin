f2698119301e0503a7ab681c106a7999d03efc01
owari-taro <taro.biwajima@gmail.com>
2020-05-10 22:54:55 +0900

    signing input

--- chapter1_updated/tx.py
+++ chapter1_updated/tx.py
@@ -101,6 +101,14 @@ class Tx:
                 return False
         return True
 
+    def sign_input(self, input_index: int, private_key):
+        z = self.sig_hash(input_index)
+        der = private_key.sign(z).der()
+        sig = der+SIGHASH_ALL.to_bytes(1, "big")
+        sec = private_key.point.sec()
+        self.tx_ins[input_index].script_sig = Script([sig, sec])
+        return self.verify_input(input_index)
+
 
 class TxIn:
     def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff):
@@ -166,3 +174,4 @@ if __name__ == "__main__":
     time.sleep(3)
     tx = Tx.parse(stream)
     #self.assertEqual(tx.version, 1)
+f
