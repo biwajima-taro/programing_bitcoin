f7f2b47e70f6607f061b5221dd8131d8b8fdfb6c
owari-taro <taro.biwajima@gmail.com>
2020-05-16 22:15:20 +0900

    fix needless encoding

--- chapter1_updated/helper.py
+++ chapter1_updated/helper.py
@@ -1,5 +1,5 @@
 import hashlib
-from script import Script
+# from script import Script
 from typing import List
 BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
@@ -21,18 +21,19 @@ def encode_base58(s: bytes) -> str:
         return prefix+result
 
 
-def hash256(s):
+def hash256(s: bytes):
     '''two rounds of sha256'''
-    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+    first = hashlib.sha256(s).digest()
+    return hashlib.sha256(first).digest()
 
 
 def encode_base58_checksum(adress: bytes):
     return encode_base58(adress+hash256(adress)[:4])
 
 
-def hash160(s):
+def hash160(s: bytes):
     '''sha256 followed by ripemd160'''
-    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()
+    return hashlib.new('ripemd160', hashlib.sha256(s.digest()).digest()
 
 
 def little_endian_to_int(b: bytes) -> int:
@@ -56,7 +57,7 @@ def int_to_little_endian(n: int, length: int):
 
 
 def read_variant(s):
-    i = s.read(1)[0]
+    i=s.read(1)[0]
     if i == 0xfd:
         return little_endian_to_int(s.read(2))
 
@@ -75,67 +76,52 @@ def encode_variant(length_: int):
 
 
 def decode_base58(s):
-    num = 0
+    num=0
     for c in s:
         num *= 58
         # index returns index  of c
         num += BASE58_ALPHABET.index(c)
-    combined = num.to_bytes(25, byteorder="big")
-    checksum = combined[-4:]
-    actual = hash256(combined[:-4])[:4]
+    combined=num.to_bytes(25, byteorder="big")
+    checksum=combined[-4:]
+    actual=hash256(combined[:-4])[:4]
     if actual != checksum:
         raise ValueError("bad address!  {actual}!={checksum}")
     return combined
 
 
-def p2pkh_script(h160: bytes):
-    """[summary]
-    return p2pkh script pubkey
-    Parameters
-    ----------
-    h160 : bytes
-        [description]
-
-    Returns
-    -------
-    [type]
-        [description]
-    """
-    return Script([0x76, 0xa9, h160, 0x88, 0xac])
-
 
 def h160_to_p2pkh_address(h160, testnet=False):
     if testnet:
-        prefix = b"\x6f"
+        prefix=b"\x6f"
     else:
-        prefix = b"\x00"
+        prefix=b"\x00"
     return encode_base58_checksum(prefix+h160)
 
 
 def h160_to_p2sh_address(h160, testnet=False):
     if testnet:
-        prefix = "\xc4"
+        prefix="\xc4"
     else:
-        prefix = "\x05"
+        prefix="\x05"
     return encode_base58_checksum(prefix+h160)
 
 
 def bits_to_target(bits: bytes) -> int:
-    exponent = bits[-1]
-    coef = little_endian_to_int(bits[:-1])
+    exponent=bits[-1]
+    coef=little_endian_to_int(bits[:-1])
     return coef*256**(exponent-3)
 
 
 def target_to_bits(target):
-    raw_bytes = target.to_bytes(32, "big")
-    raw_bytes = raw_bytes.lstrip(b"\x00")
+    raw_bytes=target.to_bytes(32, "big")
+    raw_bytes=raw_bytes.lstrip(b"\x00")
     if raw_bytes[0] > 0x7f:
-        exponent = len(raw_bytes)+1
-        coef = b"\x00" + raw_bytes[:2]
+        exponent=len(raw_bytes)+1
+        coef=b"\x00" + raw_bytes[:2]
     else:
-        exponent = len(raw_bytes)
-        coef = raw_bytes[:3]
-    new_bits = coef[::-1]+bytes([exponent])
+        exponent=len(raw_bytes)
+        coef=raw_bytes[:3]
+    new_bits=coef[::-1]+bytes([exponent])
     return new_bitws
 
 
@@ -168,22 +154,22 @@ def merkle_parent_level(hashes: List) -> List:
             "cannot take a parent level from a list with only 1 element")
     if len(hashes) % 2 == 1:
         hashes.append(hashes[-1])
-    parent_level = []
+    parent_level=[]
     for i in range(0, len(hashes), 2):
-        parent = merkle_parent(hashes[i], hashes[i+1])
+        parent=merkle_parent(hashes[i], hashes[i+1])
         parent_level.append(parent)
     return parent_level
 
 
 def merkle_root(hashes: List):
-    current_level = hashes
+    current_level=hashes
     while len(current_level) > 1:
-        current_level = merkle_parent_level(current_level)
+        current_level=merkle_parent_level(current_level)
     return current_level[0]
 
 
 def bytes_to_bit_field(some_bytes: bytes) -> List[int]:
-    flag_bits = []
+    flag_bits=[]
     for byte in some_bytes:
         for _ in range(8):
             flag_bits.append(byte & 1)
